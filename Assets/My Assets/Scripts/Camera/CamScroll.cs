0627d3324dfd33852ff00369e0a668b2757595bc
Ben Bohannon <bbohannon8@gatech.edu>
2016-01-29 12:09:37 -0500

    Door and Camera Scroll
    Created the Door script and prefab, and the Camera scrolling script.
    Both in the DemoScene.

--- Assets/My Assets/Scripts/Camera/CamScroll.cs
+++ Assets/My Assets/Scripts/Camera/CamScroll.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Camera))]
 public class CamScroll : MonoBehaviour {
+
+    private Camera myCamera;
+    private Rect screenRect;
+
+    public float scrollSpeed = 0.2f;
 
 	// Use this for initialization
 	void Start () {
-	
+        myCamera = GetComponent<Camera>();
+        screenRect = new Rect(0, 0, Screen.width, Screen.height);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        if (!screenRect.Contains(Input.mousePosition))
+        {
+            return;
+        }
+
+        Vector2 mousePos = myCamera.ScreenToViewportPoint(Input.mousePosition);
+
+	    if (mousePos.x < 0.05)
+        {
+            Vector3 currentPos = transform.position;
+            transform.position = new Vector3(currentPos.x - scrollSpeed, currentPos.y, currentPos.z);
+        }
+        else if (mousePos.x > 0.95)
+        {
+            Vector3 currentPos = transform.position;
+            transform.position = new Vector3(currentPos.x + scrollSpeed, currentPos.y, currentPos.z);
+        }
+
+        if (mousePos.y < 0.05)
+        {
+            Vector3 currentPos = transform.position;
+            transform.position = new Vector3(currentPos.x, currentPos.y - scrollSpeed, currentPos.z);
+        }
+        else if (mousePos.y > 0.95)
+        {
+            Vector3 currentPos = transform.position;
+            transform.position = new Vector3(currentPos.x, currentPos.y + scrollSpeed, currentPos.z);
+        }
 	}
 }
---
+++ Assets/My Assets/Scripts/Puzzles/InPlaceDoor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Collider2D))]
+public class InPlaceDoor : Activatable {
+
+    private SpriteRenderer myRenderer;
+    private Collider2D myCollider;
+
+    public bool isOpen = false;
+    public Sprite openSprite;
+    private Sprite closedSprite;
+
+    void Start()
+    {
+        myRenderer = GetComponent<SpriteRenderer>();
+        myCollider = GetComponent<Collider2D>();
+
+        closedSprite = myRenderer.sprite;
+
+        if (isOpen)
+        {
+            myRenderer.sprite = openSprite;
+            myCollider.enabled = false;
+        }
+    }
+
+    override public void Activate()
+    {
+        //If we're closing.
+        if (isOpen)
+        {
+            myRenderer.sprite = closedSprite;
+            myCollider.enabled = true;
+        }
+        //Else, we're opening.
+        else
+        {
+            myRenderer.sprite = openSprite;
+            myCollider.enabled = false;
+        }
+
+        isOpen = !isOpen;
+    }
+}
