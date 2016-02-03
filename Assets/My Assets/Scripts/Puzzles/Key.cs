340bbc95416b3979badcfbaa5252984867c6dc2a
William Broniec <williambroniec@gmail.com>
2016-02-03 18:24:20 -0500

    Keys and Doors
    Key becomes child of unit that picks it up, match identifiers on key and
    locked door to unlock

---
+++ Assets/My Assets/Scripts/Puzzles/Key.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class Key : MonoBehaviour
+{
+    //Identifier must match corresponding Door
+    public int Identifier;
+    private static Vector2 position;
+    private Collider2D myCollider;
+
+    void Start()
+    {
+        //This will need tochange if character size is modified
+        position = new Vector2(0, 1.3f);
+        myCollider = this.GetComponent<Collider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        //Only allies can activate and ally cannot be holding another object
+        if (coll.gameObject.tag == "Ally" && coll.gameObject.transform.childCount == 0)
+        {
+            this.transform.parent = coll.gameObject.transform;
+            this.transform.localPosition = position;
+            myCollider.enabled = false;
+        }
+    }
+}
+
---
+++ Assets/My Assets/Scripts/Puzzles/LockedDoor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Collider2D))]
+public class LockedDoor : MonoBehaviour
+{
+    //Identifier must match corresponding Key
+    public int Identifier;
+    private SpriteRenderer myRenderer;
+    private Collider2D myCollider;
+
+    public bool isOpen = false;
+    public Sprite openSprite;
+
+    void Start()
+    {
+        myRenderer = GetComponent<SpriteRenderer>();
+        myCollider = GetComponent<Collider2D>();
+
+        if (isOpen)
+        {
+            myRenderer.sprite = openSprite;
+            myCollider.enabled = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        //Only allies can activate 
+        if (coll.gameObject.tag == "Ally")
+        {
+            //Character is holding an item
+            if (coll.gameObject.transform.childCount == 1)
+            {
+                //Check if item is correct key
+                if (coll.gameObject.GetComponentInChildren<Key>().Identifier == this.Identifier)
+                {
+                    myRenderer.sprite = openSprite;
+                    myCollider.enabled = false;
+                    Destroy(coll.gameObject.transform.GetChild(0).gameObject);
+                }
+            }
+        }
+    }
+}
