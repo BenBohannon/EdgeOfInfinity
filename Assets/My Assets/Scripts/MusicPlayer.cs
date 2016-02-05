a1709e7ec4229429a8cd1f8aace111479ce53698
Jennifer Ma <jma10blue@yahoo.com>
2016-02-04 21:44:33 -0500

    added music

---
+++ Assets/My Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+	
+	void Awake() {
+//		Debug.Log ("music player AWAKE " + GetInstanceID());
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("duplicate music player: self-destruct!");
+		} else {
+			instance = this;
+		}
+		GameObject.DontDestroyOnLoad(gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+//		Debug.Log ("music player START " + GetInstanceID());
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
