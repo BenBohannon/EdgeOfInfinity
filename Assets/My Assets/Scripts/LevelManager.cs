b9780ca75ab215b849d4bd7351a66c54e1367504
Jennifer Ma <jma10blue@yahoo.com>
2016-02-04 21:43:55 -0500

    added levelmanager, fonts.meta

---
+++ Assets/My Assets/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string levelName) {
+		Debug.Log ("level load requested for level: " + levelName);
+		Application.LoadLevel(levelName);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log ("quit requested");
+		Application.Quit();
+	}
+}
