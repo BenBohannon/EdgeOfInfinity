44335e3e3782be3d59721bec98e3c5af5355ed0d
Matthew <mielusic3@gatech.edu>
2016-03-23 14:33:32 -0400

    Implemented a LevelAmbientMusic system
    Created a Prefab, “BoilerplateStuff”, in the Prefabs/Required Objects
    folder.  Inserting it into a scene will give it Pause and Music
    capability.  The MusicLooper prefab will store the location of all .mp3
    files in the Resources/AmbientLevelMusic folder, and play them randomly
    (if inserted into the current scene through the BoilerplateStuff
    prefab).  If you want to use non-.mp3 scripts, you’ll have the change
    the MusicLooper script.
    I also included a few short clips from Cave Story; we should probably
    replace them at some point.

---
+++ Assets/My Assets/Scripts/MusicLooper.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MusicLooper : MonoBehaviour {
+
+	//Will store all ambient music clips.  Start () will load every music file in the file PathName.
+	public List<FileInfo> musicFiles = new List<FileInfo> ();
+	//The audio source that will play music.  This script will load random music clips into the source at run time.
+	public AudioSource defaultSource;
+
+	//We don't want to repeat songs; store the previous song's index into musicFiles here.
+	private int previousIndex = -1;
+	//Path to the file music files will be in.  Due to how Unity loads files, songs MUST be somewhere in Assets/Resources.
+	private readonly string pathName = "Assets/Resources/LevelAmbientMusic";
+	//Resoures.Load() assumes its path its relative to the Resources directory.  Put that path here.
+	private readonly string pathInResources = "LevelAmbientMusic/";
+
+	/*
+	 * If you want to use non-.mp3 music files, add the string "*.xxx" to this array, where .xxx is the
+	 * extension you want.
+	 */
+	private readonly string[] fileExtensions = {"*.mp3"};
+
+
+	//Load the path to each music file into musicFiles.
+	void Start () {
+		DirectoryInfo musicDir = new DirectoryInfo (pathName);
+		foreach (string extension in fileExtensions) {
+			foreach (FileInfo fi in musicDir.GetFiles (extension)) {
+				musicFiles.Add (fi);
+			}
+		}
+	}
+
+	//Take action if no music is playing.
+	void Update () {
+		if (!defaultSource.isPlaying) { //If the music stopped, randomly choose a new clip.
+			//But first, unload the current clip
+			Resources.UnloadAsset(defaultSource.clip);
+			int index;
+			do {
+				index = Random.Range (0, musicFiles.Count);
+			} while (index == previousIndex);
+			previousIndex = index;
+			string path = pathInResources + Path.GetFileNameWithoutExtension(musicFiles[index].ToString ());
+			//Resources.Load expects a path relative to the Resources directory, that has no extension.
+			defaultSource.clip = Resources.Load(path) as AudioClip;
+			defaultSource.Play ();
+		}
+	}
+}
