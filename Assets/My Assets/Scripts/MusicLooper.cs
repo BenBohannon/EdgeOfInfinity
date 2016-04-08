5d8df8879af4e6d8dd71e1e4d59278e0315b6575
Ben Bohannon <bbohannon8@gatech.edu>
2016-04-08 00:07:07 -0400

    Fixed MusicLooper and Level3
    Music Looper ONLY worked in the editor, but now works for builds. I left
    in an extra cat in level 3, but took it out now.

--- Assets/My Assets/Scripts/MusicLooper.cs
+++ Assets/My Assets/Scripts/MusicLooper.cs
@@ -5,15 +5,15 @@ using System.Collections.Generic;
 
 public class MusicLooper : MonoBehaviour {
 
-	//Will store all ambient music clips.  Start () will load every music file in the file PathName.
-	public List<FileInfo> musicFiles = new List<FileInfo> ();
 	//The audio source that will play music.  This script will load random music clips into the source at run time.
 	public AudioSource defaultSource;
+
+    public List<AudioClip> audioClips = new List<AudioClip>();
 
 	//We don't want to repeat songs; store the previous song's index into musicFiles here.
 	private int previousIndex = -1;
 	//Path to the file music files will be in.  Due to how Unity loads files, songs MUST be somewhere in Assets/Resources.
-	private readonly string pathName = "Assets/Resources/LevelAmbientMusic";
+	private readonly string pathName = "LevelAmbientMusic";
 	//Resoures.Load() assumes its path its relative to the Resources directory.  Put that path here.
 	private readonly string pathInResources = "LevelAmbientMusic/";
 
@@ -26,27 +26,22 @@ public class MusicLooper : MonoBehaviour {
 
 	//Load the path to each music file into musicFiles.
 	void Start () {
-		DirectoryInfo musicDir = new DirectoryInfo (pathName);
-		foreach (string extension in fileExtensions) {
-			foreach (FileInfo fi in musicDir.GetFiles (extension)) {
-				musicFiles.Add (fi);
-			}
-		}
+        foreach(AudioClip o in Resources.LoadAll<AudioClip>(pathName))
+        {
+            audioClips.Add(o);
+        }
 	}
 
 	//Take action if no music is playing.
 	void Update () {
 		if (!defaultSource.isPlaying) { //If the music stopped, randomly choose a new clip.
-			//But first, unload the current clip
-			Resources.UnloadAsset(defaultSource.clip);
 			int index;
 			do {
-				index = Random.Range (0, musicFiles.Count);
-			} while (index == previousIndex && musicFiles.Count > 1);
+				index = Random.Range (0, audioClips.Count);
+			} while (index == previousIndex && audioClips.Count > 1);
 			previousIndex = index;
-			string path = pathInResources + Path.GetFileNameWithoutExtension(musicFiles[index].ToString ());
 			//Resources.Load expects a path relative to the Resources directory, that has no extension.
-			defaultSource.clip = Resources.Load(path) as AudioClip;
+            defaultSource.clip = audioClips[index];
 			defaultSource.Play ();
 		}
 	}
