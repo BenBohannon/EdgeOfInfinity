724c83061d6c91de8be3fab549c9f54afac75e7d
Ben Bohannon <bbohannon8@gatech.edu>
2016-04-07 23:00:36 -0400

    Pause Menu Functionality Added
    All working except Controls screen.

---
+++ Assets/My Assets/Scripts/GUI/PauseMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject optionsMenuObject;
+
+    public void optionsClick()
+    {
+        optionsMenuObject.SetActive(true);
+    }
+
+    public void controlsClick()
+    {
+
+    }
+
+    public void exitClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+}
